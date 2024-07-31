d35fbb1b2c0ce701720a60bfbc423793886ce34f
ajay2512 <bhakarajay033@gmail.com>
2024-07-31 22:35:06 +0530

    context-api

---
+++ context-api/src/App.jsx
@@ -0,0 +1,17 @@
+import Login from './components/Login'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+function App() {
+ 
+
+  return (
+    <UserContextProvider>
+     <h1>context API</h1>
+     <Login />
+     <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
